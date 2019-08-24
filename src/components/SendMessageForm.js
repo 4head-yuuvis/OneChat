2f0acfa8b6a6e5845f61a8f6273a2a67b3e53c30
Jinho Yoon <yykuk2003@gmail.com>
2019-08-24 14:18:15 -0500

    testing if remote changed

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -48,7 +48,7 @@ class SendMessageForm extends Component {
           <form onSubmit={this.onSubmit} style={styles.form}>
             <input
               type="text"
-              placeholder="commit test"
+              placeholder="checking if remote changed"
               onChange={this.onChange}
               value={this.state.text}
               style={styles.input}
